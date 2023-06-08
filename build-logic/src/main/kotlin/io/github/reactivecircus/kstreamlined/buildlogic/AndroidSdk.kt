12ddcea96cf49fe38f37de9365e52f72ead60fd3
Yang <ychescale9@gmail.com>
2023-06-08 11:37:33 +1000

    Update compileSdkVersion to 34.

--- build-logic/src/main/kotlin/io/github/reactivecircus/kstreamlined/buildlogic/AndroidSdk.kt
+++ build-logic/src/main/kotlin/io/github/reactivecircus/kstreamlined/buildlogic/AndroidSdk.kt
@@ -3,6 +3,6 @@ package io.github.reactivecircus.kstreamlined.buildlogic
 object AndroidSdk {
     const val minSdk = 24
     const val targetSdk = 33
-    const val compileSdk = 33
+    const val compileSdk = 34
     const val buildTools = "34.0.0"
 }
