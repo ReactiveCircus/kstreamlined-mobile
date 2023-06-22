a85ad30dce793d759d27a54d82066ea02ff27bc0
Yang <ychescale9@gmail.com>
2023-06-22 10:31:14 +1000

    Update Firebase, AndroidX. Kotlin 1.8.22. Bump targetSdk to 34.

--- build-logic/src/main/kotlin/io/github/reactivecircus/kstreamlined/buildlogic/AndroidSdk.kt
+++ build-logic/src/main/kotlin/io/github/reactivecircus/kstreamlined/buildlogic/AndroidSdk.kt
@@ -2,7 +2,7 @@ package io.github.reactivecircus.kstreamlined.buildlogic
 
 object AndroidSdk {
     const val minSdk = 24
-    const val targetSdk = 33
+    const val targetSdk = 34
     const val compileSdk = 34
     const val buildTools = "34.0.0"
 }
