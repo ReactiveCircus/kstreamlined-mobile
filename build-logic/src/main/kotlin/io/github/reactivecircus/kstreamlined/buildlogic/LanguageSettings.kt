bde446988ac6be0f7bb6690af46fd2b4db813e0b
Yang <ychescale9@gmail.com>
2023-08-26 15:35:08 +1000

    Enable K2 for `build-logic`, update language version to 2.0.

--- build-logic/src/main/kotlin/io/github/reactivecircus/kstreamlined/buildlogic/LanguageSettings.kt
+++ build-logic/src/main/kotlin/io/github/reactivecircus/kstreamlined/buildlogic/LanguageSettings.kt
@@ -4,7 +4,6 @@ import org.jetbrains.kotlin.gradle.plugin.LanguageSettingsBuilder
 
 internal fun LanguageSettingsBuilder.applyLanguageSettings(): LanguageSettingsBuilder {
     return apply {
-        languageVersion = "1.8"
         progressiveMode = true
         optIn("kotlin.RequiresOptIn")
     }
