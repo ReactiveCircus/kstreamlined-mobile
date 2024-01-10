4226a1df61b3c92ad5c2c37e18154d13d46c6f25
Yang <ychescale9@gmail.com>
2024-01-10 19:49:12 +0800

    Coil 3.0.0-alpha02.

--- android/app/src/main/java/io/github/reactivecircus/kstreamlined/android/di/AppModule.kt
+++ android/app/src/main/java/io/github/reactivecircus/kstreamlined/android/di/AppModule.kt
@@ -3,9 +3,6 @@ package io.github.reactivecircus.kstreamlined.android.di
 import android.content.Context
 import android.os.Build
 import coil3.ImageLoader
-import coil3.annotation.ExperimentalCoilApi
-import coil3.decode.SvgDecoder
-import coil3.fetch.NetworkFetcher
 import coil3.request.allowHardware
 import coil3.request.crossfade
 import dagger.Module
@@ -23,11 +20,6 @@ object AppModule {
     @Singleton
     fun imageLoader(@ApplicationContext context: Context): ImageLoader {
         return ImageLoader.Builder(context)
-            .components {
-                @OptIn(ExperimentalCoilApi::class)
-                add(NetworkFetcher.Factory())
-                add(SvgDecoder.Factory())
-            }
             .crossfade(enable = true)
             // only enable hardware bitmaps on API 28+. See: https://github.com/coil-kt/coil/issues/159
             .allowHardware(enable = Build.VERSION.SDK_INT >= Build.VERSION_CODES.P)
