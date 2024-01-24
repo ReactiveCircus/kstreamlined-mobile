9b554ca942d6fdeba16d1406fd340f11db3ea4cb
Yang <ychescale9@gmail.com>
2024-01-24 22:40:33 +1100

    Add mock seek bar UI for `PodcastPlayer`.

--- android/feature/talking-kotlin-episode/src/main/java/io/github/reactivecircus/kstreamlined/android/feature/talkingkotlinepisode/component/PodcastPlayer.kt
+++ android/feature/talking-kotlin-episode/src/main/java/io/github/reactivecircus/kstreamlined/android/feature/talkingkotlinepisode/component/PodcastPlayer.kt
@@ -6,14 +6,19 @@ import androidx.compose.animation.fadeOut
 import androidx.compose.animation.togetherWith
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -91,9 +96,42 @@ internal fun PodcastPlayer(
                     text = episode.displayablePublishTime,
                     style = KSTheme.typography.bodySmall,
                 )
-            }
 
-            Spacer(modifier = Modifier.width(8.dp))
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(4.dp)
+                        .clip(CircleShape)
+                ) {
+                    Surface(
+                        modifier = Modifier.fillMaxSize(),
+                        color = KSTheme.colorScheme.onBackgroundVariant,
+                    ) {}
+                    @Suppress("MagicNumber")
+                    Surface(
+                        modifier = Modifier
+                            .fillMaxWidth(0.3f)
+                            .fillMaxHeight(),
+                        color = KSTheme.colorScheme.onContainerInverse,
+                    ) {}
+                }
+
+                Row {
+                    Text(
+                        text = "24:03",
+                        style = KSTheme.typography.labelSmall,
+                        color = KSTheme.colorScheme.onTertiaryVariant,
+                    )
+
+                    Spacer(modifier = Modifier.weight(1f))
+
+                    Text(
+                        text = "-32:36",
+                        style = KSTheme.typography.labelSmall,
+                        color = KSTheme.colorScheme.onTertiaryVariant,
+                    )
+                }
+            }
 
             AnimatedContent(
                 targetState = isPlaying,
