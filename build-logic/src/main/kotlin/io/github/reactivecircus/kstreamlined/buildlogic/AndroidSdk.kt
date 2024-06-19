0f36996f9377386355a2573ddce599d231dfb6a3
Yang <ychescale9@gmail.com>
2024-06-19 11:24:50 +1000

    Update compile and target sdk to API 35, build tools 35.0.0.

--- build-logic/src/main/kotlin/io/github/reactivecircus/kstreamlined/buildlogic/AndroidSdk.kt
+++ build-logic/src/main/kotlin/io/github/reactivecircus/kstreamlined/buildlogic/AndroidSdk.kt
@@ -2,7 +2,7 @@ package io.github.reactivecircus.kstreamlined.buildlogic
 
 object AndroidSdk {
     const val minSdk = 24
-    const val targetSdk = 34
-    const val compileSdk = 34
-    const val buildTools = "34.0.0"
+    const val targetSdk = 35
+    const val compileSdk = 35
+    const val buildTools = "35.0.0"
 }
