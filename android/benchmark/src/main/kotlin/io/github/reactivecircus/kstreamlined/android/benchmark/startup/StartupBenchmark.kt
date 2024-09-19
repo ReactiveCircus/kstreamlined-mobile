b0949a6a34d9987311eb3f3ba163fa4f17927092
Yang <reactivecircus@gmail.com>
2024-09-19 18:10:21 +1000

    Enable macro benchmark startup insights, update baseline profiles (#235)
    * Update baseline profiles.
    
    * Enable startup insights.

--- android/benchmark/src/main/kotlin/io/github/reactivecircus/kstreamlined/android/benchmark/startup/StartupBenchmark.kt
+++ android/benchmark/src/main/kotlin/io/github/reactivecircus/kstreamlined/android/benchmark/startup/StartupBenchmark.kt
@@ -1,10 +1,14 @@
 package io.github.reactivecircus.kstreamlined.android.benchmark.startup
 
+import androidx.benchmark.ExperimentalBenchmarkConfigApi
+import androidx.benchmark.ExperimentalConfig
+import androidx.benchmark.StartupInsightsConfig
 import androidx.benchmark.macro.BaselineProfileMode
 import androidx.benchmark.macro.CompilationMode
 import androidx.benchmark.macro.StartupMode
 import androidx.benchmark.macro.StartupTimingMetric
 import androidx.benchmark.macro.junit4.MacrobenchmarkRule
+import androidx.benchmark.perfetto.ExperimentalPerfettoCaptureApi
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
 import io.github.reactivecircus.kstreamlined.android.benchmark.PackageName
@@ -28,10 +32,14 @@ class StartupBenchmark {
     fun startupCompilationBaselineProfiles() =
         startup(CompilationMode.Partial(BaselineProfileMode.Require))
 
+    @OptIn(ExperimentalBenchmarkConfigApi::class, ExperimentalPerfettoCaptureApi::class)
     private fun startup(compilationMode: CompilationMode) = rule.measureRepeated(
         packageName = PackageName,
         metrics = listOf(StartupTimingMetric()),
         compilationMode = compilationMode,
+        experimentalConfig = ExperimentalConfig(
+            startupInsightsConfig = StartupInsightsConfig(isEnabled = true)
+        ),
         startupMode = StartupMode.COLD,
         iterations = 10,
         setupBlock = {
