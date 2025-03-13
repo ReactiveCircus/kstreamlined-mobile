ca1844c384add569e819f1f5b9d24809f4a770e0
Yang <ychescale9@gmail.com>
2025-03-14 10:54:53 +1100

    Compile against and target API 36.

--- build-logic/kstreamlined-gradle-plugin/src/main/kotlin/io/github/reactivecircus/kstreamlined/gradle/buildlogic/AndroidSdk.kt
+++ build-logic/kstreamlined-gradle-plugin/src/main/kotlin/io/github/reactivecircus/kstreamlined/gradle/buildlogic/AndroidSdk.kt
@@ -2,7 +2,7 @@ package io.github.reactivecircus.kstreamlined.gradle.buildlogic
 
 internal object AndroidSdk {
     const val minSdk = 26
-    const val targetSdk = 35
-    const val compileSdk = 35
-    const val buildTools = "35.0.0"
+    const val targetSdk = 36
+    const val compileSdk = 36
+    const val buildTools = "36.0.0"
 }
