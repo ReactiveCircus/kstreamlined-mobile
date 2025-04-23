820e448844cf0697b885cda06250e14f26ae6b38
Yang <reactivecircus@gmail.com>
2025-04-23 11:59:34 +1000

    Paparazzi 2.0.0-alpha01. (#295)

--- android/foundation/screenshot-testing/paparazzi/build.gradle.kts
+++ android/foundation/screenshot-testing/paparazzi/build.gradle.kts
@@ -1,6 +1,20 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     id("kstreamlined.kotlin.jvm")
     id("kstreamlined.compose")
+}
+
+tasks.withType<KotlinCompile>().configureEach {
+    compilerOptions {
+        jvmTarget.set(JvmTarget.JVM_17)
+    }
+}
+
+tasks.withType<JavaCompile>().configureEach {
+    sourceCompatibility = JavaVersion.VERSION_17.toString()
+    targetCompatibility = JavaVersion.VERSION_17.toString()
 }
 
 dependencies {
