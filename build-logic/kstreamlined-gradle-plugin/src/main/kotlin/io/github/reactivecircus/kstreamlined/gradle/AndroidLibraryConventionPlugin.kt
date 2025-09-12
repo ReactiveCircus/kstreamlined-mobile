6461d4d988c2b40670c0e7b99936e43857ae87e7
Yang <ychescale9@gmail.com>
2025-09-12 12:09:04 +1000

    Enable AGP built-in Kotlin.
    - Configure all language feature and experimental annotaiton optins through `KotlinCompilationTask<*>`'s `compilerOptions` instead of `languageSettings` which only works when a kotlin plugin is explicitly applied.
    - use `kotlin-test-junit` for Android library modules.

--- build-logic/kstreamlined-gradle-plugin/src/main/kotlin/io/github/reactivecircus/kstreamlined/gradle/AndroidLibraryConventionPlugin.kt
+++ build-logic/kstreamlined-gradle-plugin/src/main/kotlin/io/github/reactivecircus/kstreamlined/gradle/AndroidLibraryConventionPlugin.kt
@@ -15,7 +15,6 @@ internal class AndroidLibraryConventionPlugin : Plugin<Project> {
     override fun apply(target: Project) = with(target) {
         with(pluginManager) {
             apply("com.android.library")
-            apply("org.jetbrains.kotlin.android")
         }
 
         extensions.configure(KotlinAndroidProjectExtension::class.java) {
