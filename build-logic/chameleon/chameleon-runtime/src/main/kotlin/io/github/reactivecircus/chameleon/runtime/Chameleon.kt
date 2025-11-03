3c4773c1f31691b70a78b9bd6d99e09038010541
Yang <ychescale9@gmail.com>
2025-11-03 13:35:00 +1100

    Add docs.

--- build-logic/chameleon/chameleon-runtime/src/main/kotlin/io/github/reactivecircus/chameleon/runtime/Chameleon.kt
+++ build-logic/chameleon/chameleon-runtime/src/main/kotlin/io/github/reactivecircus/chameleon/runtime/Chameleon.kt
@@ -1,7 +1,12 @@
 package io.github.reactivecircus.chameleon.runtime
 
 /**
- * TODO
+ * Marks a test class for automatic theme variant injection in snapshot function calls.
+ * The compiler plugin adds a `themeVariant` property to the class, which enables
+ * Burst (https://github.com/cashapp/burst) to generate parameterized tests for each theme variant.
+ * The `themeVariant` property is automatically injected into snapshot function calls
+ * (e.g., `snapshotTester.snapshot()`).
+ * The same class must also be annotated with the `@Burst` annotation.
  */
 @Target(AnnotationTarget.CLASS)
 @Retention(AnnotationRetention.SOURCE)
