018a06fd8883e72b79838560835feea400a83f68
Yang <ychescale9@gmail.com>
2025-12-02 10:50:41 +1100

    Fix test class name.

--- kmp/app-info/src/commonTest/kotlin/io/github/reactivecircus/kstreamlined/kmp/appinfo/LicensesInfoTest.kt
+++ kmp/app-info/src/commonTest/kotlin/io/github/reactivecircus/kstreamlined/kmp/appinfo/LicensesInfoTest.kt
@@ -3,7 +3,7 @@ package io.github.reactivecircus.kstreamlined.kmp.appinfo
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
-class AppInfoTest {
+class LicensesInfoTest {
     @Test
     fun `LicensesInfo can be created from JSON`() {
         val licensesInfo = LicensesInfo.fromJson {
