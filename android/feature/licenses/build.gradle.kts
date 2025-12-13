3bd3551ffae5814dff05c098ac4e12e79cd484c1
Yang <ychescale9@gmail.com>
2025-12-13 14:03:42 +1100

    Add `licentia-runtime` dependency to `:feature:licenses`.

--- android/feature/licenses/build.gradle.kts
+++ android/feature/licenses/build.gradle.kts
@@ -10,6 +10,7 @@ kstreamlined {
         dependencies {
             implementation(project(":kmp:presentation:common"))
             implementation(project(":kmp:app-info"))
+            implementation(libs.licentia.runtime)
 
             testImplementation(libs.kotlinx.coroutines.test)
             testImplementation(libs.turbine)
--- build-logic/licentia/licentia-runtime/build.gradle.kts
+++ build-logic/licentia/licentia-runtime/build.gradle.kts
@@ -7,6 +7,8 @@ plugins {
     alias(libs.plugins.detekt)
 }
 
+group = project.property("GROUP") as String
+
 kotlin {
     jvm {
         compilerOptions {
