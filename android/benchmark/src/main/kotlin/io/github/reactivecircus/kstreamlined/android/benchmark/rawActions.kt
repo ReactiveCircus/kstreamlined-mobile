7805c0c89c0aa1eb21dd345e7ce567f9981df82b
Yang <ychescale9@gmail.com>
2025-12-18 16:05:14 +1100

    Move to new UI automator DSL. Update baseline profiles.

--- android/benchmark/src/main/kotlin/io/github/reactivecircus/kstreamlined/android/benchmark/rawActions.kt
+++ android/benchmark/src/main/kotlin/io/github/reactivecircus/kstreamlined/android/benchmark/rawActions.kt
@@ -3,38 +3,16 @@
 package io.github.reactivecircus.kstreamlined.android.benchmark
 
 import android.graphics.Point
-import androidx.test.uiautomator.BySelector
 import androidx.test.uiautomator.Direction
-import androidx.test.uiautomator.UiDevice
 import androidx.test.uiautomator.UiObject2
-import androidx.test.uiautomator.Until
-import co.touchlab.kermit.Logger
-import java.io.ByteArrayOutputStream
 
-fun UiDevice.waitAndFindObject(selector: BySelector, timeoutMs: Long): UiObject2 {
-    if (!wait(Until.hasObject(selector), timeoutMs)) {
-        Logger.e(dumpWindowHierarchy())
-        throw AssertionError("Element not found on screen in ${timeoutMs}ms (selector=$selector)")
-    }
-
-    return findObject(selector)
+fun UiObject2.scrollDown() {
+    setGestureMarginPercentage(0.2f)
+    drag(Point(visibleCenter.x, -visibleCenter.y * 2))
 }
 
-fun UiDevice.scrollDown(element: UiObject2) {
-    element.setGestureMarginPercentage(0.2f)
-    element.drag(Point(element.visibleCenter.x, -element.visibleCenter.y * 2))
-    waitForIdle()
-}
-
-fun UiDevice.flingDownUp(element: UiObject2) {
-    element.setGestureMarginPercentage(0.2f)
-    element.fling(Direction.DOWN)
-    waitForIdle()
-    element.fling(Direction.UP)
-}
-
-fun UiDevice.dumpWindowHierarchy(): String {
-    val buffer = ByteArrayOutputStream()
-    dumpWindowHierarchy(buffer)
-    return buffer.toString()
+fun UiObject2.flingDownUp() {
+    setGestureMarginPercentage(0.2f)
+    fling(Direction.DOWN)
+    fling(Direction.UP)
 }
