9823e4180397d029d7edd6d478e998285001a7d0
Yang <ychescale9@gmail.com>
2026-01-22 17:42:34 +1100

    Generate overridden install function signatures.

--- build-logic/routebinding/routebinding-compiler-plugin/src/main/kotlin/io/github/reactivecircus/routebinding/compiler/ir/RouteBindingFunctionTransformer.kt
+++ build-logic/routebinding/routebinding-compiler-plugin/src/main/kotlin/io/github/reactivecircus/routebinding/compiler/ir/RouteBindingFunctionTransformer.kt
@@ -8,29 +8,37 @@ import io.github.reactivecircus.routebinding.compiler.ir.symbols.Nav3Symbols
 import io.github.reactivecircus.routebinding.compiler.ir.symbols.RouteBindingSymbols
 import org.jetbrains.kotlin.backend.common.IrElementTransformerVoidWithContext
 import org.jetbrains.kotlin.backend.common.extensions.IrPluginContext
+import org.jetbrains.kotlin.backend.common.lower.DeclarationIrBuilder
 import org.jetbrains.kotlin.descriptors.ClassKind
 import org.jetbrains.kotlin.descriptors.DescriptorVisibilities
 import org.jetbrains.kotlin.descriptors.Modality
 import org.jetbrains.kotlin.ir.IrStatement
 import org.jetbrains.kotlin.ir.UNDEFINED_OFFSET
+import org.jetbrains.kotlin.ir.builders.declarations.addFunction
 import org.jetbrains.kotlin.ir.builders.declarations.buildClass
+import org.jetbrains.kotlin.ir.builders.declarations.buildValueParameter
+import org.jetbrains.kotlin.ir.builders.irBlockBody
 import org.jetbrains.kotlin.ir.declarations.IrClass
 import org.jetbrains.kotlin.ir.declarations.IrDeclarationOrigin
 import org.jetbrains.kotlin.ir.declarations.IrFile
 import org.jetbrains.kotlin.ir.declarations.IrFunction
+import org.jetbrains.kotlin.ir.declarations.IrParameterKind
 import org.jetbrains.kotlin.ir.expressions.IrAnnotation
 import org.jetbrains.kotlin.ir.expressions.impl.IrAnnotationImpl
 import org.jetbrains.kotlin.ir.expressions.impl.IrClassReferenceImpl
 import org.jetbrains.kotlin.ir.expressions.impl.fromSymbolOwner
 import org.jetbrains.kotlin.ir.types.IrTypeSystemContextImpl
+import org.jetbrains.kotlin.ir.types.createType
 import org.jetbrains.kotlin.ir.types.defaultType
 import org.jetbrains.kotlin.ir.types.typeWith
 import org.jetbrains.kotlin.ir.util.addChild
 import org.jetbrains.kotlin.ir.util.addFakeOverrides
 import org.jetbrains.kotlin.ir.util.constructors
 import org.jetbrains.kotlin.ir.util.createThisReceiverParameter
+import org.jetbrains.kotlin.ir.util.defaultType
 import org.jetbrains.kotlin.ir.util.hasAnnotation
 import org.jetbrains.kotlin.name.Name
+import org.jetbrains.kotlin.name.SpecialNames
 
 internal class RouteBindingFunctionTransformer(
     private val pluginContext: IrPluginContext,
@@ -45,21 +53,18 @@ internal class RouteBindingFunctionTransformer(
             return super.visitFunctionNew(declaration)
         }
 
-        val navEntryInstallerClass = createNavEntryInstallerClass(declaration, currentFile)
+        val navEntryInstallerClass = declaration.createNavEntryInstallerClass(currentFile)
         currentFile.addChild(navEntryInstallerClass)
 
         return super.visitFunctionNew(declaration)
     }
 
-    private fun createNavEntryInstallerClass(
-        function: IrFunction,
-        file: IrFile,
-    ): IrClass {
+    private fun IrFunction.createNavEntryInstallerClass(file: IrFile): IrClass {
         return pluginContext.irFactory.buildClass {
             startOffset = UNDEFINED_OFFSET
             endOffset = UNDEFINED_OFFSET
             origin = IrDeclarationOrigin.DEFINED
-            name = Name.identifier("${function.name.asString()}_NavEntryInstaller")
+            name = Name.identifier("${this@createNavEntryInstallerClass.name.asString()}_NavEntryInstaller")
             kind = ClassKind.CLASS
             modality = Modality.FINAL
             visibility = DescriptorVisibilities.PUBLIC
@@ -69,6 +74,7 @@ internal class RouteBindingFunctionTransformer(
             annotations += createMetroAnnotation()
             createThisReceiverParameter()
             createDefaultConstructor(pluginContext)
+            addInstallFunction()
             addFakeOverrides(IrTypeSystemContextImpl(pluginContext.irBuiltIns))
         }
     }
@@ -86,6 +92,50 @@ internal class RouteBindingFunctionTransformer(
                 symbol = appScopeClassSymbol,
                 classType = appScopeClassSymbol.defaultType,
             )
+        }
+    }
+
+    private fun IrClass.addInstallFunction() = addFunction {
+        name = Name.identifier("install")
+        visibility = DescriptorVisibilities.PUBLIC
+        modality = Modality.OPEN
+        returnType = pluginContext.irBuiltIns.unitType
+    }.apply {
+        parameters = listOf(
+            buildValueParameter(this) {
+                name = SpecialNames.THIS
+                type = this@addInstallFunction.defaultType
+                kind = IrParameterKind.DispatchReceiver
+            },
+            buildValueParameter(this) {
+                name = Name.identifier("entryProviderScope")
+                type = nav3Symbols.entryProviderScopeClass.createType(
+                    hasQuestionMark = false,
+                    arguments = listOf(nav3Symbols.navKeyInterface.defaultType),
+                )
+                kind = IrParameterKind.Context
+            },
+            buildValueParameter(this) {
+                name = Name.identifier("sharedTransitionScope")
+                type = composeSymbols.sharedTransitionScopeInterface.defaultType
+                kind = IrParameterKind.Context
+            },
+            buildValueParameter(this) {
+                name = Name.identifier("backStack")
+                type = nav3Symbols.navBackStackClass.createType(
+                    hasQuestionMark = false,
+                    arguments = listOf(nav3Symbols.navKeyInterface.defaultType),
+                )
+                kind = IrParameterKind.Regular
+            },
+        )
+        overriddenSymbols = listOf(routeBindingSymbols.installFunction)
+        body = DeclarationIrBuilder(
+            generatorContext = pluginContext,
+            symbol = symbol,
+            startOffset = symbol.owner.startOffset,
+            endOffset = symbol.owner.endOffset,
+        ).irBlockBody {
         }
     }
 }
--- build-logic/routebinding/routebinding-compiler-plugin/src/main/kotlin/io/github/reactivecircus/routebinding/compiler/ir/symbols/RouteBindingSymbols.kt
+++ build-logic/routebinding/routebinding-compiler-plugin/src/main/kotlin/io/github/reactivecircus/routebinding/compiler/ir/symbols/RouteBindingSymbols.kt
@@ -6,11 +6,15 @@ import org.jetbrains.kotlin.backend.common.extensions.IrPluginContext
 import org.jetbrains.kotlin.cli.common.messages.CompilerMessageSeverity
 import org.jetbrains.kotlin.cli.common.messages.MessageCollector
 import org.jetbrains.kotlin.ir.symbols.IrClassSymbol
+import org.jetbrains.kotlin.ir.symbols.IrSimpleFunctionSymbol
+import org.jetbrains.kotlin.name.CallableId
 import org.jetbrains.kotlin.name.ClassId
+import org.jetbrains.kotlin.name.Name
 
 internal class RouteBindingSymbols private constructor(
     val routeBindingAnnotation: IrClassSymbol,
     val navEntryInstallerInterface: IrClassSymbol,
+    val installFunction: IrSimpleFunctionSymbol,
 ) {
     companion object {
         fun create(
@@ -35,10 +39,25 @@ internal class RouteBindingSymbols private constructor(
                 )
                 return null
             }
+            val installFunctionCallableId = CallableId(
+                classId = navEntryInstallerClassId,
+                callableName = Name.identifier("install"),
+            )
+            val installFunction = pluginContext.finderForBuiltins().findFunctions(
+                installFunctionCallableId,
+            ).singleOrNull()
+            if (installFunction == null) {
+                messageCollector.report(
+                    CompilerMessageSeverity.ERROR,
+                    "Could not find function <$installFunctionCallableId>.",
+                )
+                return null
+            }
 
             return RouteBindingSymbols(
                 routeBindingAnnotation = routeBindingAnnotation,
                 navEntryInstallerInterface = navEntryInstallerInterface,
+                installFunction = installFunction,
             )
         }
     }
