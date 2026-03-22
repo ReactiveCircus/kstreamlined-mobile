43cd6d44bd60d2b74c23b011a07736322581706e
Yang <reactivecircus@gmail.com>
2026-03-22 23:12:04 +1100

    Chameleon - remove FIR dumps. (#533)

--- build-logic/chameleon/chameleon-compiler-plugin/src/test/kotlin/io/github/reactivecircus/chameleon/compiler/AbstractBoxTest.kt
+++ build-logic/chameleon/chameleon-compiler-plugin/src/test/kotlin/io/github/reactivecircus/chameleon/compiler/AbstractBoxTest.kt
@@ -6,7 +6,6 @@ import org.jetbrains.kotlin.test.directives.CodegenTestDirectives.DUMP_IR
 import org.jetbrains.kotlin.test.directives.CodegenTestDirectives.IGNORE_DEXING
 import org.jetbrains.kotlin.test.directives.ConfigurationDirectives.WITH_STDLIB
 import org.jetbrains.kotlin.test.directives.FirDiagnosticsDirectives.DISABLE_GENERATED_FIR_TAGS
-import org.jetbrains.kotlin.test.directives.FirDiagnosticsDirectives.FIR_DUMP
 import org.jetbrains.kotlin.test.directives.JvmEnvironmentConfigurationDirectives.FULL_JDK
 import org.jetbrains.kotlin.test.directives.JvmEnvironmentConfigurationDirectives.JVM_TARGET
 import org.jetbrains.kotlin.test.runners.codegen.AbstractFirLightTreeBlackBoxCodegenTest
@@ -31,7 +30,6 @@ open class AbstractBoxTest : AbstractFirLightTreeBlackBoxCodegenTest() {
             +IGNORE_DEXING // Avoids loading R8 from the classpath.
             +DISABLE_GENERATED_FIR_TAGS
 
-            +FIR_DUMP
             +DUMP_IR
         }
     }
