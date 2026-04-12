f3596ef591550100861083c967a3620d53038598
Yang <ychescale9@gmail.com>
2026-04-12 16:17:39 +1000

    Remove `UpdateDaemonJvm` task configuration - configuration time significantly longer without specifying `--jvm-version` and `--jvm-vendor` in cli.

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -2,8 +2,3 @@ plugins {
     alias(libs.plugins.kotlin.jvm) apply false
     alias(libs.plugins.kotlin.multiplatform) apply false
 }
-
-tasks.named<UpdateDaemonJvm>("updateDaemonJvm") {
-    languageVersion.set(JavaLanguageVersion.of(26))
-    vendor.set(JvmVendorSpec.AZUL)
-}
