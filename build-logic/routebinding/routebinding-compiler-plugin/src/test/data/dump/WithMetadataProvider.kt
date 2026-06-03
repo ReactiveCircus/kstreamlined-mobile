8af028bfff539040fc1f2e513d75575c052cf305
Yang <ychescale9@gmail.com>
2026-06-03 16:01:19 +1000

    Rename test double.

--- build-logic/routebinding/routebinding-compiler-plugin/src/test/data/dump/WithMetadataProvider.kt
+++ build-logic/routebinding/routebinding-compiler-plugin/src/test/data/dump/WithMetadataProvider.kt
@@ -5,11 +5,11 @@ import androidx.navigation3.runtime.NavKey
 import io.github.reactivecircus.routebinding.runtime.RouteBinding
 import io.github.reactivecircus.routebinding.runtime.RouteMetadataProvider
 
-object TestMetadataProvider : RouteMetadataProvider {
+object DummyMetadataProvider : RouteMetadataProvider {
     override fun provide(): Map<String, Any> = mapOf("key" to "value")
 }
 
-@RouteBinding(DummyRoute::class, metadataProvider = TestMetadataProvider::class)
+@RouteBinding(DummyRoute::class, metadataProvider = DummyMetadataProvider::class)
 @Composable
 internal fun SharedTransitionScope.FooScreen(
     backStack: NavBackStack<NavKey>,
